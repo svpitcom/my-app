3b3704cdfa9162c14169880a5cbe94a6da74ff1c
Thawatchai Rueanin <55486678+Guitarolo@users.noreply.github.com>
2025-07-04 23:09:56 +0700

    Update-Layout

--- src/app/Abouts.tsx
+++ src/app/Abouts.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Abouts() {
   return (
-    <div>Abouts</div>
-  )
+    <section className="bg-gradient-to-r from-white via-gray-100 to-white py-16">
+      <div className="text-center">
+        <h2 className="text-2xl font-semibold text-gray-700">เกี่ยวกับเรา</h2>
+        <p className="text-gray-500 mt-2">เราคือทีมงานมืออาชีพ...</p>
+      </div>
+    </section>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import OurProduct from "@/app/OurProduct";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <header className="bg-sky-50 shadow p-8 flex justify-between items-center">
+      <h1 className="text-xl font-bold">SV Polymer</h1>
+      <nav className="space-x-4">
+        <a href="" className="hover:text-blue-600">
+          Home
+        </a>
+        <Link href="/our-company" className="hover:text-blue-600">
+          OurCompany
+        </Link>
+        <a href="" className="hover:text-blue-600">
+          OurProduct
+        </a>
+        <a href="" className="hover:text-blue-600">
+          Policy
+        </a>
+
+        <a href="" className="hover:text-blue-600">
+          New&Media
+        </a>
+        <a href="#services" className="hover:text-blue-600">
+          Job Opportunity
+        </a>
+        <a href="#contact" className="hover:text-blue-600">
+          Contanct Us
+        </a>
+      </nav>
+    </header>
+  );
 }
