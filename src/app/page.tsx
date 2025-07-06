49d7d28288e23a887b6136c4fc1c42876f430315
Thawatchai Rueanin <55486678+Guitarolo@users.noreply.github.com>
2025-07-07 00:13:07 +0700

    Update Foorter (:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+// import Footer from "@/components/Footer";
 // import Abouts from "./Abouts";
 import OurProduct from "./OurProdutct/page";
 import Policy from "./Policy/page";
@@ -10,7 +11,7 @@ import Router from "next/router";
 
 export default function Home() {
   return (
-    <div className="d-flex bg-[url('/assets/imgs/Tree-back.png')] bg-cover bg-center h-200 w-full">
+    <div className="d-flex bg-[url('/assets/imgs/Tree-back.png')] bg-cover bg-center h-full w-full">
       {/* <Navbar /> */}
       <main className="flex justify-center">
         <div className="relative">
@@ -27,57 +28,27 @@ export default function Home() {
               width={800}
               height={640}
             />
-            <h1>SSSSS</h1>
           </div>
         </div>
       </main>
-      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
+      <section className="bg-neutral-50 text-black py-20 px-6 text-center">
+        <h1 className="text-4xl md:text-5xl font-bold mb-4">
+          SV POLYMER INTRODUTIONS
+        </h1>
+        <p className="text-lg md:text-xl mb-6">
+          “SUSTAINABLE ECO-QUALITY RUBBER” SV Polymer Co., Ltd. is a european
+          base joint venture BOI approved natural rubber processing and export
+          company established since 2010. With strong strategic partnership of a
+          world class tire maker and an experience natural rubber processor that
+          share common vision to lead the different and be the benchmark through
+          synergy in natural rubber industry.Our goal is to create an innovative
+          material via improvement in product quality to satisfy beyond
+          customer’s expectation. Sustainability is key to Nteq, in which we
+          strive to become the cleanest and most eco-friendly factory that help
+          improves the standard living of the people and community.
+        </p>
+      </section>
+      
     </div>
   );
 }
