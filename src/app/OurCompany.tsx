652d8697e002d4542ee59dc07d285808b9dc7af4
Thawatchai Rueanin <55486678+Guitarolo@users.noreply.github.com>
2025-07-06 13:54:59 +0700

    deleted Abouts deleted OurCompany file OurCompany modified layout,page,Navbar

--- src/app/OurCompany.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OurCompany() {
-  return (
-    <div>OurCompany</div>
-  )
-}
---
+++ src/app/OurCompany/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OurCompany() {
+  return <h1>เกี่ยวกับบริษัทของเรา</h1>;
+}
