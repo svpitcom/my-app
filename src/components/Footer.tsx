175b17383ff67982f8e4b29b7872d7241d29285f
Thawatchai Rueanin <55486678+Guitarolo@users.noreply.github.com>
2025-07-07 21:42:18 +0700

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,51 +3,36 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex justify-stretch items-center bg-sky-700 py-20 columns-4">
-      <div className="flex flex-col justify-start text-white p-4">
-        <Image
-          src="/assets/imgs/LOGO_SVP_White.png"
-          alt="Company Logo"
-          width={260}
-          height={240}
-        />
-        <p className="text-sm">
-          888 Moo 6, Nampee sub-district, Thongseankhan district, Uttaradit
-          53230
-          <br />
-          Tel: +66(0)55 409 686
-        </p>
+    <footer className="row-start-3 flex justify-stretch items-center bg-sky-700 py-10 columns-4">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 items-center">
+        <div className="flex flex-col justify-start text-white p-4">
+          <Image
+            src="/assets/imgs/LOGO_SVP_White.png"
+            alt="Company Logo"
+            width={600}
+            height={500}
+          />
+          <p className="text-xl">
+            888 Moo 6, Nampee sub-district, Thongseankhan district, Uttaradit
+            53230
+            <br />
+            Tel: +66(0)55 409 686
+          </p>
+        </div>
+        <div className="text-center text-white font-semibold text-3xl border-y md:border-y-0 md:border-x border-white py-2">
+          CUSTOMER CASE
+        </div>
+        <div className="text-center ">
+          <h2 className="font-bold text-3xl text-white">
+            SV POLYMER
+            <br />
+            INTRANET
+          </h2>
+          <button className="mt-3 px-4 py-2 bg-white text-blue-800 font-semibold rounded hover:bg-gray-100 flex items-center justify-center gap-2 mx-auto">
+            Member Login
+          </button>
+        </div>
       </div>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/window.svg"
-          alt="Window icon"
-          width={16}
-          height={16}
-        />
-        Examples
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          aria-hidden
-          src="/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        Go to nextjs.org →
-      </a>
     </footer>
   );
 }
