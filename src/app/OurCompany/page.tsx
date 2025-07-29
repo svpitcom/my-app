5ada0a1de652560d3233098b1ae44935b590567e
Thawatchai Rueanin <thawatchairueanin5@gmail.com>
2025-07-29 21:41:17 +0700

    xzz

--- src/app/OurCompany/page.tsx
+++ src/app/OurCompany/page.tsx
@@ -4,152 +4,153 @@ import AnimateOnScroll from "@/components/AnimateOnScroll";
 
 export default function OurCompany() {
   return (
-    <div className="bg-gradient-to-br from-sky-600 to-green-500 bg-cover bg-center">
-      <div className="bg-gradient-to-b bg-white/20 via-white to-white bg-cover bg-center text-white h-full py-2">
-        <div className="py-20">
-          <h1 className="text-white text-shadow-lg/20 text-center text-6xl font-bold mb-4 py-10">
-            About Us
-          </h1>
-          <div className="max-w-4xl mx-auto">
-            <div className="text-gray-800 p-10 rounded-lg text-balance">
-              <h1 className="text-3xl font-bold text-sky-700 py-4">
-                SV Polymer CO., LTD. (SV Polymer){" "}
+    // <div className="bg-gradient-to-b bg-white/20 via-white to-white bg-cover bg-center text-white h-full py-2">
+    <div>
+      <div className="bg-[url('/assets/imgs/DJI_0673.jpg')] bg-cover bg-center text-white w-full relative py-60">
+        
+      </div>
+      <div className="bg-gradient-to-br from-sky-600 to-green-500 bg-cover bg-center py-10">
+        <h1 className="text-white text-shadow-lg/20 text-center text-6xl font-bold mb-4 py-10">
+          About Us
+        </h1>
+        <div className="max-w-4xl mx-auto">
+          <div className="text-gray-800 p-10 rounded-lg text-balance">
+            <h1 className="text-3xl font-bold text-sky-700 py-4">
+              SV Polymer CO., LTD. (SV Polymer){" "}
+            </h1>
+            <p className="text-base mt-2">
+              is a European based joint venture BOI approved natural rubber
+              processing and export company established in 26th July 2010. With
+              combined experience of over 150 years in natural rubber business
+              from world class tire maker (Michelin) and experience natural
+              rubber processor (Mr. Chee Wan Lee, Mr. Payungsak Kerdvonbundit)
+              who shared common vision to create sustainable value added product
+              and service through research and development.
+            </p>
+            <br />
+            <br />
+            <p className="text-base mt-2">
+              We are headquartered with 252 ha (1,600 rais) of land in a
+              strategic location, Uttaradit, Thailand the center of east west
+              economic corridor that connects 4 countries Myanmar, Thailand,
+              Laos and Vietnam which produces total estimation of 47% of world
+              rubber production and raw material sources.
+            </p>
+
+            <br />
+            <br />
+            <p className="text-base mt-2">
+              To achieve sustainability in the natural rubber industry it is
+              essential to continually develop process efficiency and quality,
+              maintain high corporate social responsibility through breakthrough
+              innovation to adapt to the ongoing changes in the world’s economy
+              and environment.
+            </p>
+            <br />
+            <br />
+            <br />
+            <p className="text-base mt-2">
+              We understand the importance of quality and its impact on the
+              final products which may have save lives. Therefore, we are
+              committed to manufacturing rubber with stringent quality control
+              policy that our company always upheld to meet the requirements of
+              our customers.
+            </p>
+            <br />
+            <br />
+            <p className="text-base mt-2">
+              As we aim best in producing highest eco-quality standardized
+              natural rubber. To prove our commitment, we had already been
+              certified with ISO9001:2008, ISO14001:2015, TLS8001:2010, Silver
+              Medal in Ecovadis with highest score in environmental aspect,
+              Green industry award and soon ISO/IEC 17025:2005.
+            </p>
+            <br />
+            <br />
+            <p className="text-base mt-2">
+              We believe that skillful labor and employees, state-of-art
+              technology, creative methodology and strong commitment to research
+              and development will enable Nteq Polymer to be center of
+              excellence in natural rubber processing technology in the future.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Vision Box & Missions Box */}
+      <div className="flex flex-col md:flex-row justify-center mx-auto gap-6 md:gap-10 p-4 md:p-16 rounded-lg backdrop-blur-sm">
+        {/* Vision Box */}
+        <div className="bg-gradient-to-br from-sky-600 to-green-500 py-16 px-16 rounded-md shadow-md w-full h-96 md:w-1/2 text-center">
+          <h2 className="text-2xl md:text-5xl font-bold text-blue-700 mb-4">
+            Vision
+          </h2>
+          <p className="text-white text-base md:text-lg">
+            Lead the different and be the benchmark
+            <br />
+            through synergy in natural rubber industry
+          </p>
+        </div>
+
+        {/* Missions Box */}
+        <div className="bg-gradient-to-br from-sky-600 to-green-500 py-16 px-16 rounded-md shadow-md w-full md:w-1/2 text-center">
+          <h2 className="text-2xl md:text-5xl font-bold text-blue-700 mb-4">
+            Missions
+          </h2>
+          <p className="text-white text-base md:text-lg">
+            Advance technology innovative material
+            <br />
+            Create customer satisfaction and produce products at the highest
+            quality
+            <br />
+            Clean and environmentally friendly factory that help develop and
+            improve standard of living
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center mt-6">
+        <Image
+          src="/assets/imgs/Borad.png"
+          alt="Borad"
+          width={1366}
+          height={800}
+          className="w-full max-w-[1366px] object-contain"
+        />
+      </div>
+      <AnimateOnScroll>
+        <div className="flex flex-col md:flex-row w-full h-auto mt-6">
+          <div className="w-full md:w-1/2 relative h-64 md:h-auto">
+            <Image
+              src="/assets/imgs/Rectangle27.png"
+              alt="Boardroom"
+              className="object-cover"
+              fill
+            />
+          </div>
+
+          <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white flex items-center">
+            <div className="p-4 md:p-10 space-y-3">
+              <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
+                Board of Directors
               </h1>
-              <p className="text-base mt-2">
-                is a European based joint venture BOI approved natural rubber
-                processing and export company established in 26th July 2010.
-                With combined experience of over 150 years in natural rubber
-                business from world class tire maker (Michelin) and experience
-                natural rubber processor (Mr. Chee Wan Lee, Mr. Payungsak
-                Kerdvonbundit) who shared common vision to create sustainable
-                value added product and service through research and
-                development.
-              </p>
-              <br />
-              <br />
-              <p className="text-base mt-2">
-                We are headquartered with 252 ha (1,600 rais) of land in a
-                strategic location, Uttaradit, Thailand the center of east west
-                economic corridor that connects 4 countries Myanmar, Thailand,
-                Laos and Vietnam which produces total estimation of 47% of world
-                rubber production and raw material sources.
-              </p>
-
-              <br />
-              <br />
-              <p className="text-base mt-2">
-                To achieve sustainability in the natural rubber industry it is
-                essential to continually develop process efficiency and quality,
-                maintain high corporate social responsibility through
-                breakthrough innovation to adapt to the ongoing changes in the
-                world’s economy and environment.
-              </p>
-              <br />
-              <br />
-              <br />
-              <p className="text-base mt-2">
-                We understand the importance of quality and its impact on the
-                final products which may have save lives. Therefore, we are
-                committed to manufacturing rubber with stringent quality control
-                policy that our company always upheld to meet the requirements
-                of our customers.
-              </p>
-              <br />
-              <br />
-              <p className="text-base mt-2">
-                As we aim best in producing highest eco-quality standardized
-                natural rubber. To prove our commitment, we had already been
-                certified with ISO9001:2008, ISO14001:2015, TLS8001:2010, Silver
-                Medal in Ecovadis with highest score in environmental aspect,
-                Green industry award and soon ISO/IEC 17025:2005.
-              </p>
-              <br />
-              <br />
-              <p className="text-base mt-2">
-                We believe that skillful labor and employees, state-of-art
-                technology, creative methodology and strong commitment to
-                research and development will enable Nteq Polymer to be center
-                of excellence in natural rubber processing technology in the
-                future.
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* Vision Box & Missions Box */}
-        <div className="flex flex-col md:flex-row justify-center mx-auto gap-6 md:gap-10 p-4 md:p-16 rounded-lg backdrop-blur-sm">
-          {/* Vision Box */}
-          <div className="bg-gradient-to-br from-sky-600 to-green-500 py-16 px-16 rounded-md shadow-md w-full h-96 md:w-1/2 text-center">
-            <h2 className="text-2xl md:text-5xl font-bold text-blue-700 mb-4">
-              Vision
-            </h2>
-            <p className="text-white text-base md:text-lg">
-              Lead the different and be the benchmark
-              <br />
-              through synergy in natural rubber industry
-            </p>
-          </div>
-
-          {/* Missions Box */}
-          <div className="bg-gradient-to-br from-sky-600 to-green-500 py-16 px-16 rounded-md shadow-md w-full md:w-1/2 text-center">
-            <h2 className="text-2xl md:text-5xl font-bold text-blue-700 mb-4">
-              Missions
-            </h2>
-            <p className="text-white text-base md:text-lg">
-              Advance technology innovative material
-              <br />
-              Create customer satisfaction and produce products at the highest
-              quality
-              <br />
-              Clean and environmentally friendly factory that help develop and
-              improve standard of living
-            </p>
-          </div>
-        </div>
-
-        <div className="flex justify-center items-center mt-6">
-          <Image
-            src="/assets/imgs/Borad.png"
-            alt="Borad"
-            width={1366}
-            height={800}
-            className="w-full max-w-[1366px] object-contain"
-          />
-        </div>
-        <AnimateOnScroll>
-          <div className="flex flex-col md:flex-row w-full h-auto mt-6">
-            <div className="w-full md:w-1/2 relative h-64 md:h-auto">
-              <Image
-                src="/assets/imgs/Rectangle27.png"
-                alt="Boardroom"
-                className="object-cover"
-                fill
-              />
-            </div>
-
-            <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white flex items-center">
-              <div className="p-4 md:p-10 space-y-3">
-                <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
-                  Board of Directors
-                </h1>
-                <div className="space-y-3">
-                  <div className="space-y-2 text-left md:text-left text-sm md:text-base">
-                    <p className="font-semibold">Mr. Chaipon Kerdvonbundit</p>
-                    <p>Chief Executive Officer</p>
-                    <p className="font-semibold">Mrs. Wei-Mei Wang</p>
-                    <p>Executive Director</p>
-                    <p className="font-semibold">Mr. Gnoh Tong Tan</p>
-                    <p>Executive Director</p>
-                    <p className="font-semibold">Mr. Segsarn Trai Ukos</p>
-                    <p>Executive Director</p>
-                    <p className="font-semibold">Mr. Sethasit Nitayakul</p>
-                    <p>Executive Director</p>
-                  </div>
+              <div className="space-y-3">
+                <div className="space-y-2 text-left md:text-left text-sm md:text-base">
+                  <p className="font-semibold">Mr. Chaipon Kerdvonbundit</p>
+                  <p>Chief Executive Officer</p>
+                  <p className="font-semibold">Mrs. Wei-Mei Wang</p>
+                  <p>Executive Director</p>
+                  <p className="font-semibold">Mr. Gnoh Tong Tan</p>
+                  <p>Executive Director</p>
+                  <p className="font-semibold">Mr. Segsarn Trai Ukos</p>
+                  <p>Executive Director</p>
+                  <p className="font-semibold">Mr. Sethasit Nitayakul</p>
+                  <p>Executive Director</p>
                 </div>
               </div>
             </div>
           </div>
-        </AnimateOnScroll>
-      </div>
+        </div>
+      </AnimateOnScroll>
+      {/* // </div> */}
     </div>
   );
 }
--- src/app/Policy/page.tsx
+++ src/app/Policy/page.tsx
@@ -53,7 +53,7 @@ export default function Policy() {
       </div>
 
       <AnimateOnScroll>
-        <div className="bg-gradient-to-br from-sky-600 to-green-500 text-white text-white py-10 text-center px-4">
+        <div className="bg-gradient-to-br from-sky-600 to-green-500 text-white py-10 text-center px-4">
           <h1 className="text-3xl md:text-6xl font-bold mb-3">
             Energy Conservation Policy
           </h1>
@@ -80,7 +80,7 @@ export default function Policy() {
                   key={index}
                   className="w-52 h-52 md:w-72 md:h-72 rounded-full bg-gradient-to-br from-sky-600/60 to-green-500/60 text-white flex items-center justify-center text-center px-4"
                 >
-                  <div className="w-44 h-44 md:w-64 md:h-64 rounded-full bg-gradient-to-br from-sky-600 to-green-500 text-white text-blue-500 flex items-center justify-center text-xs md:text-base font-bold p-4">
+                  <div className="w-44 h-44 md:w-64 md:h-64 rounded-full bg-gradient-to-br from-sky-600 to-green-500 text-white flex items-center justify-center text-xs md:text-base font-bold p-4">
                     {text}
                   </div>
                 </div>
