05cd6d871196813644f839867be81520fa55cdf6
Thawatchai Rueanin <thawatchairueanin5@gmail.com>
2025-08-06 23:41:40 +0700

    Update

--- src/app/JobOpportunity/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function JobOpportunity() {
-  return (
-    <div className="flex items-center justify-center  bg-cover bg-center min-h-screen text-center">
-      <h1 className="text-lg">JobOpportunity</h1>
-    </div>
-  )
-}
---
+++ src/types/home.ts
@@ -0,0 +1,12 @@
+export interface HomeData {
+  home_id: number;
+  home_title_en: string;
+  home_detail_01_en: string;
+  home_detail_02_en: string;
+  home_detail_03_en: string;
+  home_title_th: string;
+  home_detail_01_th: string;
+  home_detail_02_th: string;
+  home_detail_03_th: string;
+  created_at: string; // หรือ Date ถ้าคุณแปลงเป็น Date object
+}
