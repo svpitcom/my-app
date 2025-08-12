1f149d94d730fb3121b32d5ecda3a59821cfff68
Thawatchai Rueanin <thawatchairueanin5@gmail.com>
2025-08-12 23:58:29 +0700

    Update

--- src/app/[locale]/OurCompany/page.tsx
+++ src/app/[locale]/OurCompany/page.tsx
@@ -47,15 +47,9 @@ export default function OurCompany() {
   }, [lang]);
 
   return (
-    <div className="bg-gradient-to-br from-sky-600 to-green-500 bg-cover bg-center">
-      <div
-        className="
-                    bg-[url('/assets/imgs/Ourcompanyv3.png')]
-                    sm:bg-[url('/assets/imgs/Ourcompanyv3.png')]
-                    bg-cover bg-center text-white w-full relative py-20
-                  "
-      >
-        <div className="flex flex-col text-center items-center px-4 py-4">
+    <div className="containers">
+      <div className="bg-[url('/assets/imgs/Ourcompanyv3.png')] sm:bg-[url('/assets/imgs/Ourcompanyv3.png')] bg-cover bg-center text-white relative py-20">
+        <div className="flex flex-col text-center items-center px-4 py-6">
           <Image
             src="/assets/imgs/AW_LOGO_SVP [Final] + Stroke-01.png"
             alt="LogoSVP"
@@ -63,141 +57,145 @@ export default function OurCompany() {
             height={180}
             className="mb-4 w-40 h-40 md:w-64 md:h-64 object-contain"
           />
-        </div>
-        {data?.map((item) => (
-          <h1
-            key={item.our_company_id}
-            className="text-white text-shadow-lg/20 text-center text-3xl md:text-6xl font-bold mb-4 py-6 md:py-6"
-          >
-            {getValue(
-              item,
-              `our_company_title_${lang}` as keyof OurCompanyData
-            )}
-          </h1>
-        ))}
-      </div>
-      <div className="max-w-4xl mx-auto">
-        {data?.map((item) => (
-          <div
-            key={item.our_company_id}
-            className="text-white p-10 rounded-lg text-balance"
-          >
-            <h1 className="text-xl md:text-3xl font-bold text-white py-2 md:py-4">
+          {data?.map((item) => (
+            <h1
+              key={item.our_company_id}
+              className="text-white text-shadow-lg/20 text-center text-3xl md:text-6xl font-bold mb-4 py-2 md:py-6"
+            >
               {getValue(
                 item,
-                `our_company_subtitle_${lang}` as keyof OurCompanyData
+                `our_company_title_${lang}` as keyof OurCompanyData
               )}
             </h1>
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_01_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-            <br />
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_02_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-            <br />
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_03_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-            <br />
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_04_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-            <br />
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_05_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-            <br />
-            <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
-              {getValue(
-                item,
-                `our_company_detail_06_${lang}` as keyof OurCompanyData
-              )}
-            </p>
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-col md:flex-row justify-center gap-6 md:gap-10 p-4 md:p-16 rounded-lg backdrop-blur-sm">
-        <div className="bg-gradient-to-r from-sky-500 to-green-300 p-6 md:py-16 md:px-16 rounded-md shadow-md w-full h-auto md:h-96 text-center text-balance">
-          <h2 className="text-xl md:text-5xl font-bold text-blue-700 mb-4">
-            Vision
-          </h2>
-          <p className="text-white text-sm md:text-lg leading-relaxed">
-            Lead the different and be the benchmark through synergy in natural
-            rubber industry
-          </p>
-        </div>
-
-        <div className="bg-gradient-to-r from-sky-500 to-green-300 p-6 md:py-16 md:px-16 rounded-md shadow-md w-full h-auto md:h-96 text-center text-balance">
-          <h2 className="text-xl md:text-5xl font-bold text-blue-700 mb-4">
-            Missions
-          </h2>
-          <p className="text-white text-sm md:text-lg leading-relaxed">
-            Advance technology innovative material Create customer satisfaction
-            and produce products at the highest quality Clean and
-            environmentally friendly factory that help develop and improve
-            standard of living
-          </p>
+          ))}
         </div>
       </div>
-      <div className="bg-white w-full text-center justify-center items-center py-8">
-        <h1 className="text-xl md:text-5xl py-8 text-blue-700">Oraganization Chart</h1>
-        <Image
-          src="/assets/imgs/Borad-v1.png"
-          alt="Borad"
-          width={1366}
-          height={800}
-          className="w-full max-w-full object-contain"
-        />
-      </div>
-      <AnimateOnScroll>
-        <div className="flex flex-col md:flex-row w-full h-auto mt-6">
-          <div className="w-full md:w-1/2 relative h-64 md:h-auto">
-            <Image
-              src="/assets/imgs/Rectangle27.png"
-              alt="Boardroom"
-              className="object-cover"
-              fill
-            />
-          </div>
-          <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white flex items-center">
-            <div className="p-4 md:p-10 space-y-3">
-              <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
-                Board of Directors
+      <div className="bg-gradient-to-br from-sky-600 to-green-500">
+        <div className="max-w-4xl mx-auto">
+          {data?.map((item) => (
+            <div
+              key={item.our_company_id}
+              className="text-white p-8 rounded-lg text-balance"
+            >
+              <h1 className="text-xl md:text-3xl font-bold text-white py-2 md:py-4">
+                {getValue(
+                  item,
+                  `our_company_subtitle_${lang}` as keyof OurCompanyData
+                )}
               </h1>
-              <div className="space-y-3">
-                <div className="space-y-2 text-left md:text-left text-sm md:text-base">
-                  <p className="font-semibold">Mr. Chaipon Kerdvonbundit</p>
-                  <p>Chief Executive Officer</p>
-                  <p className="font-semibold">Mrs. Wei-Mei Wang</p>
-                  <p>Executive Director</p>
-                  <p className="font-semibold">Mr. Gnoh Tong Tan</p>
-                  <p>Executive Director</p>
-                  <p className="font-semibold">Mr. Segsarn Trai Ukos</p>
-                  <p>Executive Director</p>
-                  <p className="font-semibold">Mr. Sethasit Nitayakul</p>
-                  <p>Executive Director</p>
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_01_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+              <br />
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_02_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+              <br />
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_03_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+              <br />
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_04_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+              <br />
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_05_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+              <br />
+              <p className="text-sm md:text-base mt-2 leading-relaxed whitespace-pre-line">
+                {getValue(
+                  item,
+                  `our_company_detail_06_${lang}` as keyof OurCompanyData
+                )}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col md:flex-row justify-center gap-6 md:gap-10 p-4 md:p-16 rounded-lg backdrop-blur-sm">
+          <div className="bg-gradient-to-r from-sky-500 to-green-300 p-6 md:py-16 md:px-16 rounded-md shadow-md w-full h-auto md:h-96 text-center text-balance">
+            <h2 className="text-xl md:text-5xl font-bold text-blue-700 mb-4">
+              Vision
+            </h2>
+            <p className="text-white text-sm md:text-lg leading-relaxed">
+              Lead the different and be the benchmark through synergy in natural
+              rubber industry
+            </p>
+          </div>
+
+          <div className="bg-gradient-to-r from-sky-500 to-green-300 p-6 md:py-16 md:px-16 rounded-md shadow-md w-full h-auto md:h-96 text-center text-balance">
+            <h2 className="text-xl md:text-5xl font-bold text-blue-700 mb-4">
+              Missions
+            </h2>
+            <p className="text-white text-sm md:text-lg leading-relaxed">
+              Advance technology innovative material Create customer
+              satisfaction and produce products at the highest quality Clean and
+              environmentally friendly factory that help develop and improve
+              standard of living
+            </p>
+          </div>
+        </div>
+        <div className="bg-white w-full text-center justify-center items-center py-8">
+          <h1 className="text-xl md:text-5xl py-8 text-blue-700">
+            Oraganization Chart
+          </h1>
+          <Image
+            src="/assets/imgs/Borad-v1.png"
+            alt="Borad"
+            width={1366}
+            height={800}
+            className="w-full max-w-full object-contain"
+          />
+        </div>
+        <AnimateOnScroll>
+          <div className="flex flex-col md:flex-row w-full h-auto mt-6">
+            <div className="w-full md:w-1/2 relative h-64 md:h-auto">
+              <Image
+                src="/assets/imgs/Rectangle27.png"
+                alt="Boardroom"
+                className="object-cover"
+                fill
+              />
+            </div>
+            <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white flex items-center">
+              <div className="p-4 md:p-10 space-y-3">
+                <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
+                  Board of Directors
+                </h1>
+                <div className="space-y-3">
+                  <div className="space-y-2 text-left md:text-left text-sm md:text-base">
+                    <p className="font-semibold">Mr. Chaipon Kerdvonbundit</p>
+                    <p>Chief Executive Officer</p>
+                    <p className="font-semibold">Mrs. Wei-Mei Wang</p>
+                    <p>Executive Director</p>
+                    <p className="font-semibold">Mr. Gnoh Tong Tan</p>
+                    <p>Executive Director</p>
+                    <p className="font-semibold">Mr. Segsarn Trai Ukos</p>
+                    <p>Executive Director</p>
+                    <p className="font-semibold">Mr. Sethasit Nitayakul</p>
+                    <p>Executive Director</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </AnimateOnScroll>
+        </AnimateOnScroll>
+      </div>
     </div>
   );
 }
--- src/app/[locale]/OurProduct/page.tsx
+++ src/app/[locale]/OurProduct/page.tsx
@@ -4,126 +4,129 @@ import AnimateOnScroll from "@/components/AnimateOnScroll";
 
 export default function OurProduct() {
   return (
-    <div className="bg-gradient-to-r from-sky-500 to-green-300 bg-cover bg-center w-full py-30 sm:py-30">
-      <div className="bg-[url('/assets/imgs/OurProduction.png')] bg-cover bg-center text-white w-full relative ">
+    <div>
+      {/* bg-[url('/assets/imgs/Ourcompanyv3.png')] sm:bg-[url('/assets/imgs/Ourcompanyv3.png')] bg-cover bg-center text-white relative py-20 */}
+      <div className="bg-[url('/assets/imgs/OurProduction.png')] sm:bg-[url('/assets/imgs/OurProduction.png')] bg-cover bg-center text-white relative py-30">
         <div className="flex justify-center items-center">
           <Image
             src="/assets/imgs/AW_LOGO_SVP [Final] + Stroke-01.png"
             alt="LogoSVP"
-            width={160} // ขนาดเริ่มต้นสำหรับมือถือ
-            height={160}
-            className="mb-4 sm:w-[200px] sm:h-[200px] md:w-[240px] md:h-[240px] object-contain"
+            width={240}
+            height={240}
+            className="mb-4 w-[120px] h-[120px] sm:w-[160px] sm:h-[160px] md:w-[200px] md:h-[200px] object-contain"
           />
         </div>
       </div>
 
-      <div className="bg-cover bg-center">
-        <div className="flex justify-center py-10 md:py-12">
-          <h1 className="text-white text-shadow-lg/20 text-4xl md:text-6xl font-bold p-4 md:p-6 rounded text-center">
-            OurProducts
-          </h1>
-        </div>
-        <div className="bg-neutral-50 text-black py-10 md:py-16 px-4 md:px-10 text-center">
-          <div className="max-w-5xl mx-auto">
-            <h1 className="text-3xl md:text-4xl font-bold mb-4">
-              Technically Specified Rubber
+      <div className="bg-gradient-to-r from-sky-500 to-green-300 bg-cover bg-center w-full">
+        <div className="bg-cover bg-center">
+          <div className="flex justify-center py-10 md:py-12">
+            <h1 className="text-white text-shadow-lg/20 text-4xl md:text-6xl font-bold p-4 md:p-6 rounded text-center">
+              OurProducts
             </h1>
-            <br />
-            <p className="text-base md:text-lg text-left leading-relaxed">
-              Demand for TSR has been increasing over the years due to its
-              measurable quality and production technology which allows better
-              consistency in the rubber quality. Raw materials and finished
-              goods are tested according to quality plan to ensure the rubber
-              produced meets the required standard of our customers. SV Polymer
-              core operations are processing of Standard Thai Rubber grade
-              STR10, and STR20 as well as Mooney viscosity controlled 9710 and
-              9720.
-            </p>
-            <br />
-            <p className="text-base md:text-lg text-left leading-relaxed">
-              Customization of products or special request specifications of
-              quality are always welcome. Our further research and development
-              will lead to future production of new value added products.
-              Currently, our annual production capacity is 72,000 tons that will
-              be continuously expanded to support the growth of both domestic
-              and international demands.
-            </p>
           </div>
-        </div>
-        <AnimateOnScroll>
-          <div className="flex flex-col md:flex-row w-full rounded-lg p-10">
-            <div className="w-full md:w-1/2 relative h-60 md:h-auto">
-              <Image
-                src="/assets/imgs/inProduction.png"
-                alt="Company Logo"
-                className="object-cover rounded-lg"
-                fill
-              />
+          <div className="bg-neutral-50 text-black py-10 md:py-16 px-4 md:px-10 text-center">
+            <div className="max-w-5xl mx-auto">
+              <h1 className="text-3xl md:text-4xl font-bold mb-4">
+                Technically Specified Rubber
+              </h1>
+              <br />
+              <p className="text-base md:text-lg text-left leading-relaxed">
+                Demand for TSR has been increasing over the years due to its
+                measurable quality and production technology which allows better
+                consistency in the rubber quality. Raw materials and finished
+                goods are tested according to quality plan to ensure the rubber
+                produced meets the required standard of our customers. SV
+                Polymer core operations are processing of Standard Thai Rubber
+                grade STR10, and STR20 as well as Mooney viscosity controlled
+                9710 and 9720.
+              </p>
+              <br />
+              <p className="text-base md:text-lg text-left leading-relaxed">
+                Customization of products or special request specifications of
+                quality are always welcome. Our further research and development
+                will lead to future production of new value added products.
+                Currently, our annual production capacity is 72,000 tons that
+                will be continuously expanded to support the growth of both
+                domestic and international demands.
+              </p>
             </div>
-            <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white rounded-lg flex items-center">
-              <div className="p-6 md:p-10 space-y-3 text-left md:text-left">
-                <h2 className="text-xl md:text-2xl font-semibold mb-2">
-                  Product
-                </h2>
-                <div className="space-y-2 text-sm md:text-base">
-                  <p className="font-semibold">Standard Thai Rubber 10</p>
-                  <p className="font-semibold">Standard Thai Rubber 20</p>
-                  <p className="font-semibold">Standard Thai Rubber 20M</p>
-                  <p className="font-semibold">
-                    Mooney Viscosity Controlled 9710
-                  </p>
-                  <p className="font-semibold">
-                    Mooney Viscosity Controlled 9720
-                  </p>
-                  <p className="font-semibold">Constant Viscosity 60</p>
-                  <p className="font-semibold">Constant Viscosity 65</p>
-                  <p className="font-semibold">
-                    Mixture Rubber (Pending 2017–2018)
-                  </p>
+          </div>
+          <AnimateOnScroll>
+            <div className="flex flex-col md:flex-row w-full rounded-lg p-10">
+              <div className="w-full md:w-1/2 relative h-60 md:h-auto">
+                <Image
+                  src="/assets/imgs/inProduction.png"
+                  alt="Company Logo"
+                  className="object-cover rounded-lg"
+                  fill
+                />
+              </div>
+              <div className="w-full md:w-1/2 bg-gradient-to-br from-sky-600 to-green-500 text-white rounded-lg flex items-center">
+                <div className="p-6 md:p-10 space-y-3 text-left md:text-left">
+                  <h2 className="text-xl md:text-2xl font-semibold mb-2">
+                    Product
+                  </h2>
+                  <div className="space-y-2 text-sm md:text-base">
+                    <p className="font-semibold">Standard Thai Rubber 10</p>
+                    <p className="font-semibold">Standard Thai Rubber 20</p>
+                    <p className="font-semibold">Standard Thai Rubber 20M</p>
+                    <p className="font-semibold">
+                      Mooney Viscosity Controlled 9710
+                    </p>
+                    <p className="font-semibold">
+                      Mooney Viscosity Controlled 9720
+                    </p>
+                    <p className="font-semibold">Constant Viscosity 60</p>
+                    <p className="font-semibold">Constant Viscosity 65</p>
+                    <p className="font-semibold">
+                      Mixture Rubber (Pending 2017–2018)
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </AnimateOnScroll>
-        <AnimateOnScroll>
-          <div className="bg-white py-8 px-4 md:px-10">
-            <h2 className="text-4xl md:text-7xl font-bold text-center text-sky-700 mb-4 uppercase">
-              Packaging
-            </h2>
-            <p className="text-sm md:text-lg max-w-4xl mx-auto text-center text-black">
-              Various packaging are available to meet the requirements of our
-              customers. Our container loading is carried out in our factory
-              under close supervision to ensure that rubber arrives at the
-              destination in its best conditions.
-            </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
-              {[
-                { weight: "35", description: "kilograms loose bales" },
-                {
-                  weight: "1.26",
-                  description: "tons shrink wrapped Plastic base",
-                },
-                {
-                  weight: "1.33",
-                  description:
-                    "tons shrink wrapped metal base/Friendly pack pallets",
-                },
-                { weight: "1.26", description: "tons Metal box" },
-              ].map((item, index) => (
-                <div key={index} className="flex flex-col items-center">
-                  <div className="w-36 h-36 md:w-64 md:h-64 rounded-full bg-gradient-to-r from-sky-500/60 to-green-300/60 flex items-center justify-center">
-                    <div className="w-32 h-32 md:w-56 md:h-56 rounded-full bg-gradient-to-r from-sky-500 to-green-300 text-white flex flex-col items-center justify-center text-center p-2">
-                      <p className="text-xl md:text-3xl font-bold">
-                        {item.weight}
-                      </p>
-                      <p className="text-xs md:text-lg">{item.description}</p>
+          </AnimateOnScroll>
+          <AnimateOnScroll>
+            <div className="bg-white py-8 px-4 md:px-10">
+              <h2 className="text-4xl md:text-7xl font-bold text-center text-sky-700 mb-4 uppercase">
+                Packaging
+              </h2>
+              <p className="text-sm md:text-lg max-w-4xl mx-auto text-center text-black">
+                Various packaging are available to meet the requirements of our
+                customers. Our container loading is carried out in our factory
+                under close supervision to ensure that rubber arrives at the
+                destination in its best conditions.
+              </p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+                {[
+                  { weight: "35", description: "kilograms loose bales" },
+                  {
+                    weight: "1.26",
+                    description: "tons shrink wrapped Plastic base",
+                  },
+                  {
+                    weight: "1.33",
+                    description:
+                      "tons shrink wrapped metal base/Friendly pack pallets",
+                  },
+                  { weight: "1.26", description: "tons Metal box" },
+                ].map((item, index) => (
+                  <div key={index} className="flex flex-col items-center">
+                    <div className="w-36 h-36 md:w-64 md:h-64 rounded-full bg-gradient-to-r from-sky-500/60 to-green-300/60 flex items-center justify-center">
+                      <div className="w-32 h-32 md:w-56 md:h-56 rounded-full bg-gradient-to-r from-sky-500 to-green-300 text-white flex flex-col items-center justify-center text-center p-2">
+                        <p className="text-xl md:text-3xl font-bold">
+                          {item.weight}
+                        </p>
+                        <p className="text-xs md:text-lg">{item.description}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        </AnimateOnScroll>
+          </AnimateOnScroll>
+        </div>
       </div>
     </div>
   );
