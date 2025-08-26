18ca8d59e598b7d75067112db03b1800ad026205
svpitcom <svpit01@gmail.com>
2025-08-26 13:51:28 +0700

    ss

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // ให้ Next.js build เป็น static files
+  //output: "export", // ให้ Next.js build เป็น static files
   images: {
-    unoptimized: true, // ปิด optimizer ของ next/image
+   // ปิด optimizer ของ next/image
     domains: ["your-image-domain.com"], // ถ้ามีรูปจาก domain นอก
   },
 };
