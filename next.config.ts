013d488bf4c1a6f8ff3d36a87d45b72941a4b028
svpitcom <svpit01@gmail.com>
2025-08-26 13:27:07 +0700

    Update Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-// next.config.ts
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export", // ให้ Next.js build เป็น static files
   images: {
-    domains: ["your-image-domain.com"],
+    unoptimized: true, // ปิด optimizer ของ next/image
+    domains: ["your-image-domain.com"], // ถ้ามีรูปจาก domain นอก
   },
 };
 
