d211646ef64f82a1352401ffec87c120c5c13eb0
svpitcom <svpit01@gmail.com>
2025-08-26 16:03:52 +0700

    ๊Update Cparesel

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const { createServer } = require("http");
 const next = require("next");
 
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  }).listen(port, () => {
+    console.log(`> Server ready on http://localhost:${port}`);
   });
 });
